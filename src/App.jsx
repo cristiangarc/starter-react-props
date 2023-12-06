ebcf15f0a6e37c84984e8a029a669166e19f5618
Cristian Garcia <crisg@sas.upenn.edu>
2023-12-06 14:12:02 -0500

    added a prop to the Aside component

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header name={dayCareName} date={todaysDate} />
-      <Aside />
+      <Aside dogs={dogs} />
       <Main />
     </div>
   );
